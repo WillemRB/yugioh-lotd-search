b3bec9ecde71abf8dfbce361da7043ed1bd83bed
Willem Bouma <wrb@outlook.com>
2015-09-07 15:43:29 +0200

    Add kube and start working on templates

--- cards/views.py
+++ cards/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from models import Card
 
 def index(request):
     return render(request, 'card.html')
+    
+def search(request):
+    return render(request, 'search.html')
+    
+def single_card(request, card_id):
+    card = get_object_or_404(Card, pk=card_id)
+    return render(request, 'card.html', { 'card': card })
--- yugioh/urls.py
+++ yugioh/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', 'cards.views.index', name='home'),
+    url(r'^$', 'cards.views.search', name='search'),
     
-    url(r'^card/', 'cards.views.index', name='card'),
+    #url(r'^card/$', 'cards.views.index', name='card'),
+    url(r'^card/(\d+)$', 'cards.views.single_card', name='single_card'),
     
     url(r'^booster/(\d+)$', 'cardsources.views.view_booster', name='booster'),
 
