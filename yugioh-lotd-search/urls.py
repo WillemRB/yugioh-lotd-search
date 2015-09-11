3f133cd1563816306561e44a7d92e565fc8f9093
Willem Bouma <wrb@outlook.com>
2015-09-11 15:34:30 +0200

    Making it all work with Azure Web App

--- yugioh-lotd-search/urls.py
+++ yugioh-lotd-search/urls.py
@@ -1,14 +1,16 @@
-from django.conf.urls import patterns, include, url
+﻿from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', 'cards.views.index', name='index'),
-    
-    url(r'^search/$', 'cards.views.search', name='search'),
-    
+    url(r'^$', 'cards.views.search', name='search'),
+
+    #url(r'^search/$', 'cards.views.search', name='search'),
+
     #url(r'^card/$', 'cards.views.index', name='card'),
     url(r'^card/(?P<card_id>\d+)/$', 'cards.views.single_card', name='single_card'),
-    
+    url(r'^card/report/(?P<card_id>\d+)/submit$', 'cards.views.report_card_submit', name='submit-report'),
+    url(r'^card/report/(?P<card_id>\d+)/$', 'cards.views.report_card', name='report'),
+
     url(r'^booster/(?P<booster_id>\d+)/$', 'cardsources.views.view_booster', name='booster'),
     url(r'^deck/(?P<deck_id>\d+)/$', 'cardsources.views.view_deck', name='deck'),
 
