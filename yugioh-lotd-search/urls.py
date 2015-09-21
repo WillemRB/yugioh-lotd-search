ff11970e728385dff53e8c8bbc77719ea653b193
Willem Bouma <wrb@outlook.com>
2015-09-21 14:27:06 +0200

    Add robots.txt to urls

--- yugioh-lotd-search/urls.py
+++ yugioh-lotd-search/urls.py
@@ -1,13 +1,12 @@
 ﻿from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponse
 
 urlpatterns = patterns('',
     url(r'^$', 'cards.views.search', name='search'),
 
     url(r'^card/(?P<card_id>\d+)/(?P<slug>[-\w\d]*)/?$', 'cards.views.single_card', name='single_card'),
     url(r'^card/report/(?P<card_id>\d+)/(?P<slug>[-\w\d]*)/?$', 'cards.views.report_card', name='report'),
-    #url(r'^card/report/(?P<card_id>\d+)/submit$', 'cards.views.report_card_submit', name='submit-report'),
-    #url(r'^card/report/(?P<card_id>\d+)/$', 'cards.views.report_card', name='report'),
 
     url(r'^booster/(?P<booster_id>\d+)/(?P<slug>[-\w\d]*)/?$', 'cardsources.views.view_booster', name='booster'),
     url(r'^boosters/$', 'cardsources.views.boosters_list', name='boosters'),
@@ -16,4 +15,6 @@ urlpatterns = patterns('',
     url(r'^decks/$', 'cardsources.views.decks_list', name='decks'),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^robots.txt$', lambda r: HttpResponse('User-agent: *\nDisallow: /admin/', content_type='text/plain')),
 )
