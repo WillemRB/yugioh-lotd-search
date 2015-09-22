9c840d181e4f34b6e1b86d44426c9372ff5f79e0
Willem Bouma <wrb@outlook.com>
2015-09-22 09:29:31 +0200

    Add search fields and change display of card admin page

--- cards/admin.py
+++ cards/admin.py
@@ -3,6 +3,8 @@ from cards.models import CardType, Card
 
 class CardAdmin(admin.ModelAdmin):
     prepopulated_fields = { 'slug': ('name',) }
+    search_fields = ('name', 'database_id', 'id',)
+    list_display = ('name', 'database_id', 'id',)
     fieldsets = [
         (None,           { 'fields': ['name', 'slug', 'description', 'attribute', 'limitation', 'database_id', 'image_url', 'decks', 'boosters'] }),
         ('Monster Card', { 'fields': ['card_types', 'attack', 'defense', 'level', 'rank'] }),
